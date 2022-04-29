9dbb1604dcd8267be354ce784e9da7dcdfd4eb74
timoast <4591688+timoast@users.noreply.github.com>
2022-04-29 17:53:03 -0400

    Fix demultiplexer

--- scripts/demux.py
+++ scripts/demux.py
@@ -21,6 +21,10 @@ parser.add_argument('--output', help='Path to output directory')
 args = parser.parse_args()
 
 
+def hamming_distance(s1, s2):
+    return sum(c1 != c2 for c1, c2 in zip(s1, s2))
+
+
 def open_fastq(f):
     if os.path.isfile(f):
         if f.endswith(".gz"):
@@ -32,7 +36,18 @@ def open_fastq(f):
         raise Exception("File not found")
 
 
-def get_barcodes(inpath, maxlen=8):
+def read_sample_barcodes(inpath, maxlen=8):
+    all_bc = dict()
+    for i in SeqIO.parse(open(inpath),'fasta'):
+        all_bc[str(i.seq[:maxlen])] = i.id
+    return(all_bc)
+
+
+def read_tn5_barcodes(inpath, maxlen=8):
+    """Read barcode sequences from fasta file, splitting ID by +
+    Returns a dictionary:
+       key = sample, value = dictionary: key = barcode, value = mark
+    """
     all_bc = dict()
     for i in SeqIO.parse(open(inpath),'fasta'):
         values = i.id.split("+")
@@ -47,19 +62,14 @@ def get_barcodes(inpath, maxlen=8):
     return(all_bc)
 
 
-def read_sample_barcodes(inpath, maxlen=8):
-    all_bc = dict()
-    for i in SeqIO.parse(open(inpath),'fasta'):
-        all_bc[str(i.seq[:maxlen])] = i.id
-    return(all_bc)
-
-
 def get_entry(f):
     return([f.readline(), f.readline(), f.readline(), f.readline()])
 
 
 def extract_barcodes(sequence, adapter, full_adapter_len, bc1_len, bc2_len):
-    # return first 9 bp as tn5_bc, remove next 15 bp (adaptor), then return remaining as sample index
+    # return first x bp as tn5_bc
+    # remove next 15 bp (adaptor)
+    # return remaining as sample index
     tn5_bc = sequence[:bc1_len]
     trimmed = sequence[bc1_len+1:]
     idx_match = trimmed.find(adapter)
@@ -72,10 +82,21 @@ def extract_barcodes(sequence, adapter, full_adapter_len, bc1_len, bc2_len):
             si = si[:bc2_len]
     return((tn5_bc, si))
 
+def extract_barcodes_simple(sequence, bc1_len=8, spacer_len=14):
+    # version with fixed-length barcodes
+    sequence = sequence.strip("\n")
+    tn5_bc = sequence[:bc1_len]
+    cell_bc = sequence[bc1_len+spacer_len:-1]
+    if len(cell_bc) != 16:
+        print(cell_bc)
+    return((tn5_bc, cell_bc))
 
-tn5_i5_barcodes = get_barcodes(args.tn5_i5, maxlen=8) # using first 8 bases of barcode
-tn5_i7_barcodes = get_barcodes(args.tn5_i7, maxlen=8)
+
+tn5_i5_barcodes = read_tn5_barcodes(args.tn5_i5, maxlen=8) # using first 8 bases of barcode
+tn5_i7_barcodes = read_tn5_barcodes(args.tn5_i7, maxlen=8)
 sample_barcodes = read_sample_barcodes(args.sampleindex, maxlen=8)
+
+sample_names = tn5_i5_barcodes.keys()
 
 r1 = open_fastq(f=args.read1)
 r2 = open_fastq(f=args.read2)
@@ -88,7 +109,7 @@ outpath = Path(args.output)
 if not outpath.exists():
     os.mkdir(outpath)
 
-for sampleindex in tn5_i5_barcodes.keys():
+for sampleindex in sample_names:
     fpath = outpath / sampleindex
     if not fpath.exists():
         os.mkdir(fpath)
@@ -122,29 +143,54 @@ while True:
         break
 
     # get barcodes
-    i7_barcodes = extract_barcodes(sequence=r2_entry[1], adapter="CGAGAC", full_adapter_len=15, bc1_len=8, bc2_len=8)
-    i5_barcodes = extract_barcodes(sequence=r3_entry[1], adapter="CGACGA", full_adapter_len=14, bc1_len=8, bc2_len=16)
+    i7_barcodes = extract_barcodes(
+        sequence=r2_entry[1],
+        adapter="CGAGAC",
+        full_adapter_len=14,
+        bc1_len=8,
+        bc2_len=8
+    )
+    i5_barcodes = extract_barcodes(
+        sequence=r3_entry[1],
+        adapter="CGACGA",
+        full_adapter_len=14,
+        bc1_len=8,
+        bc2_len=16
+    )
 
+    # check if sample index sequence is in sample barcodes
     if i7_barcodes[1] in sample_barcodes:
         sample_index_name = sample_barcodes[i7_barcodes[1]]
-        # look up mark names
+    else:
+        # error-correcting
+        hams = [hamming_distance(i7_barcodes[1], y) < 3 for y in sample_barcodes.keys()]
+        if sum(hams) == 1:
+            # one unique match
+            sample_index_name = sample_barcodes[list(sample_barcodes.keys())[hams.index(True)]]
+        else:
+            sample_index_name = "unknown"
+    # look up mark names
+    if sample_index_name == "unknown":
+        i7_mark, i5_mark, well = "unknown", "unknown", "unknown"
+    else:
         if i7_barcodes[0] in tn5_i7_barcodes[sample_index_name]:
             i7_mark, well = tn5_i7_barcodes[sample_index_name][i7_barcodes[0]]
         else:
+            # TODO error correction
             i7_mark, well = "unknown", "unknown"
         if i5_barcodes[0] in tn5_i5_barcodes[sample_index_name]:
             i5_mark, well = tn5_i5_barcodes[sample_index_name][i5_barcodes[0]]
         else:
+            # TODO error correction
             i5_mark, well = "unknown", "unknown"
-    else:
-        sample_index_name, i5_mark, i7_mark, well = "unknown", "unknown", "unknown", "unknown"
 
     cell_barcode = i5_barcodes[1]
     if len(cell_barcode) >= args.min_barcode_len:
         # add barcodes to r1 and r2 genomic
-        bc_combination = "@" + cell_barcode + "-" + well + ":" + i5_barcodes[0] + "+" + i7_barcodes[0] + "+"
-        r1_entry[0] = bc_combination + r1_entry[0][1:]
-        r4_entry[0] = bc_combination + r4_entry[0][1:]
+        cb = " CB:Z:" + cell_barcode + "-" + well
+        tb = " TB:Z:" + i5_barcodes[0] + "+" + i7_barcodes[0] + "\n"
+        r1_entry[0] = r1_entry[0].strip("\n") + cb + tb
+        r4_entry[0] = r4_entry[0].strip("\n") + cb + tb
         
         if sample_index_name == "unknown":
             r1_outf = outf['unknown'][0]
